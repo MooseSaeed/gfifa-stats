1a8d45c9f16876f023072c6287cce14a81df974d
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-09-03 00:22:55 +0200

    thead extraction

---
+++ components/table/Tlayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <table class="table-auto w-full">
+    <thead>
+      <tr class="text-white text-sm leading-normal">
+        <slot name="tableHeads" />
+      </tr>
+    </thead>
+
+    <tbody class="text-sm font-light">
+      <slot name="tableRows" />
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "Tlayout",
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="container mx-auto text-white">
     <div class="overflow-x-auto mx-auto py-10 max-w-6xl">
-      <table class="table-auto w-full">
-        <thead>
-          <tr class="text-white font-light text-sm leading-normal">
-            <th class="py-3 px-6 text-center">Name</th>
-            <th class="py-3 px-6 text-center">OVR</th>
-            <th class="py-3 px-6 text-center">POS</th>
-            <th class="py-3 px-6 text-center">Type</th>
-            <th class="py-3 px-6 text-center">PAC</th>
-            <th class="py-3 px-6 text-center">SHO</th>
-            <th class="py-3 px-6 text-center">PAS</th>
-          </tr>
-        </thead>
+      <tableTlayout>
+        <template v-slot:tableHeads>
+          <th class="py-3 px-6 text-center">Name</th>
+          <th class="py-3 px-6 text-center">OVR</th>
+          <th class="py-3 px-6 text-center">POS</th>
+          <th class="py-3 px-6 text-center">Type</th>
+          <th class="py-3 px-6 text-center">PAC</th>
+          <th class="py-3 px-6 text-center">SHO</th>
+          <th class="py-3 px-6 text-center">PAS</th>
+        </template>
 
         <tbody class="text-sm font-light">
           <tr
@@ -41,7 +39,7 @@
             </td>
           </tr>
         </tbody>
-      </table>
+      </tableTlayout>
     </div>
   </div>
 </template>
