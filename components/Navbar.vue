ee65d67c1d8d964515223a69b92a5dfbf6126377
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-09-03 00:13:16 +0200

    Navbar init and assets

---
+++ components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="flex justify-between items-center">
+    <iconsMenu />
+
+    <div>
+      <img class="fill-gray-400 w-32 px-4" src="~/assets/RS-logo.png" alt="" />
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ components/icons/Menu.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="p-5">
+    <svg
+      width="18"
+      height="12"
+      viewBox="0 0 18 12"
+      fill="white"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M17 1H1"
+        stroke="white"
+        stroke-width="1.5"
+        stroke-linecap="square"
+        stroke-linejoin="bevel"
+      />
+      <path
+        d="M17 11H1"
+        stroke="white"
+        stroke-width="1.5"
+        stroke-linecap="square"
+        stroke-linejoin="bevel"
+      />
+    </svg>
+  </div>
+</template>
+
+<script></script>
