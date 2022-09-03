8b61cdd684f5bee5c3694cd760179c583c5bfe9d
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-09-03 02:46:55 +0200

    Fetch card info

--- pages/index.vue
+++ pages/index.vue
@@ -17,64 +17,74 @@
         </template>
 
         <template v-slot:tableRows>
-          <tableTrow>
+          <tableTrow v-for="(player, index) in cardsInfo" :key="index">
             <td class="py-5 px-6 text-center">
-              <p class="rounded-md inline font-normal">Messi</p>
+              <p class="rounded-md inline font-normal">{{ player.name }}</p>
             </td>
             <td class="py-5 px-6 text-center text-black">
-              <p class="bg-white rounded-md inline px-4 py-2">93</p>
+              <p class="bg-white rounded-md inline px-4 py-2">
+                {{ getOVR(player.description) }}
+              </p>
             </td>
             <td class="py-5 px-2 text-center text-white">
-              <p class="bg-black rounded-md inline px-4 py-2">RW</p>
+              <p class="bg-black rounded-md inline px-4 py-2">
+                {{ player.position }}
+              </p>
             </td>
             <td class="py-5 px-2 text-center text-white">
-              <p class="bg-black rounded-md inline px-4 py-2">RW</p>
-            </td>
-            <td class="py-5 px-1 text-center text-white">
-              <p
-                class="bg-black border-2 border-white rounded-md inline px-5 py-2"
-              >
-                85
+              <p class="bg-black rounded-md inline px-4 py-2">
+                {{ player.position }}
               </p>
             </td>
             <td class="py-5 px-1 text-center text-white">
               <p
                 class="bg-black border-2 border-white rounded-md inline px-5 py-2"
               >
-                85
+                {{ player.statistics.pace.average }}
               </p>
             </td>
             <td class="py-5 px-1 text-center text-white">
               <p
                 class="bg-black border-2 border-white rounded-md inline px-5 py-2"
               >
-                85
+                {{ player.statistics.shooting.average }}
               </p>
             </td>
             <td class="py-5 px-1 text-center text-white">
               <p
                 class="bg-black border-2 border-white rounded-md inline px-5 py-2"
               >
-                85
+                {{ player.statistics.passing.average }}
               </p>
             </td>
             <td class="py-5 px-1 text-center text-white">
               <p
                 class="bg-black border-2 border-white rounded-md inline px-5 py-2"
               >
-                85
+                {{ player.statistics.dribbling.average }}
               </p>
             </td>
             <td class="py-5 px-1 text-center text-white">
               <p
                 class="bg-black border-2 border-white rounded-md inline px-5 py-2"
               >
-                85
+                {{ player.statistics.defense.average }}
+              </p>
+            </td>
+            <td class="py-5 px-1 text-center text-white">
+              <p
+                class="bg-black border-2 border-white rounded-md inline px-5 py-2"
+              >
+                {{ player.statistics.physical.average }}
               </p>
             </td>
 
             <td class="py-5 px-2 text-center text-white">
-              <p class="bg-black rounded-md inline px-4 py-2">M/L</p>
+              <p class="bg-black rounded-md inline px-4 py-2">
+                {{
+                  getWR(player.workRatesAttacking, player.workRatesDefensive)
+                }}
+              </p>
             </td>
           </tableTrow>
         </template>
@@ -84,6 +94,7 @@
 </template>
 
 <script>
+import { client } from "../plugins/sanity.js";
 export default {
   name: "IndexPage",
   data() {
@@ -92,26 +103,27 @@ export default {
     };
   },
   async fetch() {
-    /* init Sanity client */
-    const sanityClient = require("@sanity/client");
-    /* provide required Gfinity details */
-    const client = sanityClient({
-      projectId: "21fy9g0s",
-      dataset: "production",
-      apiVersion: "2021-03-25",
-      useCdn: true,
-    });
-
     try {
       /* Get me everything about those fifa cards */
       const query = '*[_type == "fifaCard"]';
-      this.cardsInfo = await client.fetch(query).then((allCards) => {
-        return allCards;
+      await client.fetch(query).then((allCards) => {
+        this.cardsInfo = allCards;
       });
       console.log(this.cardsInfo);
     } catch (error) {
       console.log(error);
     }
+  },
+  methods: {
+    // OCR is only included in the details so I had to extract it somehow
+    getOVR(fullDescription) {
+      // Get only numbers and remove 22 (text has fifa 22 so it should be removed)
+      return fullDescription.match(/\d/g).join("").replace(/[2]/g, "");
+    },
+    // WorkRates are devided to attacking and defense
+    getWR(attack, defense) {
+      return attack.charAt(0) + "/" + defense.charAt(0);
+    },
   },
 };
 </script>
--- plugins/sanity.js
+++ plugins/sanity.js
@@ -1,18 +1,9 @@
-export default function myCoolData(query) {
-  const sanityClient = require("@sanity/client");
-  const client = sanityClient({
-    projectId: "21fy9g0s",
-    dataset: "production",
-    apiVersion: "2021-03-25",
-    useCdn: true,
-  });
+const sanityClient = require("@sanity/client");
+const client = sanityClient({
+  projectId: "21fy9g0s",
+  dataset: "production",
+  apiVersion: "2021-03-25",
+  useCdn: true,
+});
 
-  client.fetch(query).then((bikes) => {
-    console.log(bikes);
-    /*     bikes.forEach((bike) => {
-      console.log(`${bike.name} (${bike.seats} seats)`);
-    }); */
-  });
-
-  return client;
-}
+export { client };
